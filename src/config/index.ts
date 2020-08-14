860caeb7ec7916c3aa7fa313b3664a9114ef7fe5
alexreiling <alexanderreiling@googlemail.com>
2020-08-14 19:05:07 +0200

    config checks

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ if (envFound.error) {
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
-export default {
+const config = {
   /**
    * Your favorite port
    */
@@ -16,7 +16,13 @@ export default {
 
   mongoURI: process.env.MONGODB_URI,
 
-  typeormURI: process.env.TYPEORM_URI,
+  typeORM: {
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    user: process.env.TYPEORM_USER,
+    passsword: process.env.TYPEORM_PW,
+    dbName: process.env.TYPEORM_DB_NAME
+  },
 
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
@@ -26,7 +32,7 @@ export default {
    */
   // TODO: add type safety and checks
   logs: {
-    level: process.env.LOG_LEVEL || 'silly',
+    level: process.env.LOG_LEVEL || 'silly'
   },
 
   /**
@@ -35,7 +41,7 @@ export default {
   agenda: {
     dbCollection: process.env.AGENDA_DB_COLLECTION,
     pooltime: process.env.AGENDA_POOL_TIME,
-    concurrency: parseInt(process.env.AGENDA_CONCURRENCY!, 10),
+    concurrency: parseInt(process.env.AGENDA_CONCURRENCY!, 10)
   },
 
   /**
@@ -44,13 +50,14 @@ export default {
   agendash: {
     user: 'agendash',
     password: '123456',
+    test: undefined
   },
   /**
    * API configs
    */
   api: {
-    prefix: '/api',
-  },
+    prefix: '/api'
+  }
   // /**
   //  * Mailgun email credentials
   //  */
@@ -59,3 +66,15 @@ export default {
   //   domain: process.env.MAILGUN_DOMAIN,
   // },
 };
+function validateConfig<T extends { [key: string]: any }>(c: T, parents: string[] = []): T {
+  Object.keys(c).forEach(key => {
+    const value = c[key];
+    if (!value) throw new Error('missing config parameter: ' + [...parents, key].join('.'));
+    else if (typeof value === 'object') {
+      const passParents = [...parents, key];
+      validateConfig(value, passParents);
+    }
+  });
+  return c;
+}
+export default validateConfig(config);
